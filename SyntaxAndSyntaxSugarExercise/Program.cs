aeb6d7aca92288eb5f2bd5432a6339a3f35ff66c
SoapyEnigma <keithlekgolo@gmail.com>
2025-07-24 19:06:41 -0400

    Update for $

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             var answer = 4;
-            var response = answer.ToString() + (answer < 9 ? " is less than nine" : " is greater than or equal to nine");
+            //var response = answer.ToString() + (answer < 9 ? " is less than nine" : " is greater than or equal to nine");
+            var response = answer < 9 ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
         }
     }
 }
